905bd0d52cb5aac3035c0dd3ff6d7323ba7c5824
atizaz786 <atizazhashmi786@gmail.com>
2024-02-06 13:23:57 +0000

    feat: Solved the issue for conversation api and added code api and UI

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,84 @@
+"use client";
 
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import {
+  ArrowRight,
+  Code,
+  ImageIcon,
+  MessageSquare,
+  Music,
+  Video,
+} from "lucide-react";
+import { useRouter } from "next/navigation";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    href: "/conversation",
+    bgColor: "bg-violet-500/10",
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    color: "text-emerald-500",
+    href: "/music",
+    bgColor: "bg-emerald-500/10",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    color: "text-pink-700",
+    href: "/image",
+    bgColor: "bg-pink-700/10",
+  },
+  {
+    label: "Video Generation",
+    icon: Video,
+    color: "text-orange-700",
+    href: "/video",
+    bgColor: "bg-orange-700/10",
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    color: "text-green-700",
+    href: "/code",
+    bgColor: "bg-green-700/10",
+  },
+];
 
 export default function DashboardPage() {
+  const router = useRouter();
   return (
     <div>
-      <p>Dashboard Page (Protected)</p>
- 
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of AI
+        </h2>
+        <p className="text-muted-foregroound font-light text-sm md:text-lg text-center">
+          Chat with Genius to generate music, code, and more.
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("h-8 w-8", tool.color)} />
+              </div>
+              <div className="font-semibold">{tool.label}</div>
+            </div>
+            <ArrowRight className="h-6 w-6 text-black/50" />
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
