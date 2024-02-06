1b5fafba96647e964590fb31c4f127d4e45ae6e8
atizaz786 <atizazhashmi786@gmail.com>
2024-02-06 16:25:01 +0000

    feat: added database using prisma for free trial count

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -1,41 +1,48 @@
-
-
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import Replicate from "replicate";
 
+import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
+
 const replicate = new Replicate({
-    auth: process.env.REPLICATE_API_KEY || "",
+  auth: process.env.REPLICATE_API_KEY || "",
 });
 
-export async function POST (req: Request){
-    try{
-        const {userId} = auth();
-        const body = await req.json();
-        const {prompt} = body;
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt } = body;
 
-        if(!userId){
-            return new NextResponse("Unauthorized", {status: 401})
-        }
-
-
-        if(!prompt){
-            return new NextResponse("Messages are required", {status: 400})
-        }
-
-        const response = await replicate.run(
-            "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
-            {
-              input: {
-                prompt_a: prompt
-              }
-            }
-          );
-          return NextResponse.json(response)
-
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
-    catch (error){
-        console.log("[MUSIC_ERROR]", error)
-        return new NextResponse("An error occurred", {status: 500})
+
+    if (!prompt) {
+      return new NextResponse("Messages are required", { status: 400 });
     }
+
+    const freeTrial = await checkApiLimit();
+
+    if (!freeTrial) {
+      return new NextResponse("You have reached the free trial limit", {
+        status: 403,
+      });
+    }
+
+    const response = await replicate.run(
+      "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+      {
+        input: {
+          prompt_a: prompt,
+        },
+      }
+    );
+
+    await increaseApiLimit();
+    return NextResponse.json(response);
+  } catch (error) {
+    console.log("[MUSIC_ERROR]", error);
+    return new NextResponse("An error occurred", { status: 500 });
+  }
 }
---
+++ lib/api-limit.ts
@@ -0,0 +1,60 @@
+import { auth } from "@clerk/nextjs";
+
+import prismadb from "./prismadb";
+import { FREE_MAX_COUNTS } from "@/constants";
+
+export const increaseApiLimit = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return;
+  }
+
+  const userApiLimit = await prismadb.userApiLimit.findUnique({
+    where: {
+      userId,
+    },
+  });
+
+  if (userApiLimit) {
+    await prismadb.userApiLimit.update({
+      where: {
+        userId,
+      },
+      data: {
+        count: userApiLimit.count + 1,
+      },
+    });
+  } else {
+    await prismadb.userApiLimit.create({
+      data: {
+        count: 1,
+        userId,
+      },
+    });
+  }
+};
+
+
+export const checkApiLimit = async () => {
+    const { userId } = auth();
+    
+    if (!userId) {
+        return false;
+    }
+    
+    const userApiLimit = await prismadb.userApiLimit.findUnique({
+        where: {
+        userId,
+        },
+    });
+
+    if(!userApiLimit || userApiLimit.count < FREE_MAX_COUNTS){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+   
+}
