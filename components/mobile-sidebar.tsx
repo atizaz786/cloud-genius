090ffe481a1eb829805a9a162046d83480e2cc40
atizaz786 <atizazhashmi786@gmail.com>
2024-02-06 21:33:50 +0000

    feat: Added stripe for pro subscription payments

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,19 +1,29 @@
 "use client";
 
-import { useState, useEffect, use } from "react";
+import { useEffect, useState } from "react";
 import { Menu } from "lucide-react";
-import { Button } from "./ui/button";
-import { Sheet, SheetTrigger, SheetContent } from "./ui/sheet";
-import Sidebar from "./sidebar";
 
-const MobileSidebar = () => {
-    const [isMounted, setIsMounted] = useState(false);
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "./sidebar";;
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
+export const MobileSidebar = ({
+  apiLimitCount = 0,
+  isPro = false
+}: {
+  apiLimitCount: number;
+  isPro: boolean;
+}) => {
+  const [isMounted, setIsMounted] = useState(false);
 
-    if (!isMounted) return null;
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <Sheet>
       <SheetTrigger>
@@ -22,10 +32,8 @@ const MobileSidebar = () => {
         </Button>
       </SheetTrigger>
       <SheetContent side="left" className="p-0">
-        <Sidebar />
+        <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
       </SheetContent>
     </Sheet>
   );
 };
-
-export default MobileSidebar;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,21 @@
-
 import { UserButton } from "@clerk/nextjs";
-import MobileSidebar from "./mobile-sidebar";
 
-const Navbar = () => {
-  return (
+import { MobileSidebar } from "@/components/mobile-sidebar";
+import { getApiLimit } from "@/lib/api-limit";
+import { checkSubscription } from "@/lib/subscription";
+
+const Navbar = async () => {
+  const apiLimitCount = await getApiLimit();
+  const isPro = await checkSubscription();
+
+  return ( 
     <div className="flex items-center p-4">
-   <MobileSidebar/>
+      <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} />
       <div className="flex w-full justify-end">
-        <UserButton afterSignOutUrl="/"/>
-
+        <UserButton afterSignOutUrl="/" />
       </div>
-
     </div>
-  );
-};
-
+   );
+}
+ 
 export default Navbar;
