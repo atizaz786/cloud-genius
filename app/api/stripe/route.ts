de76efe3074989801bd724a0bfe41dcd0d5e9863
atizaz786 <atizazhashmi786@gmail.com>
2024-02-07 20:12:10 +0000

    feat: Completed stripe integration

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server";
 
 import prismadb from "@/lib/prismadb";
 import { stripe } from "@/lib/stripe";
-
 import { absoluteUrl } from "@/lib/utils";
 
 const settingsUrl = absoluteUrl("/settings");
@@ -13,23 +12,23 @@ export async function GET() {
     const { userId } = auth();
     const user = await currentUser();
 
-    if (!user || !userId) {
+    if (!userId || !user) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
     const userSubscription = await prismadb.userSubscription.findUnique({
       where: {
-        userId,
-      },
-    });
+        userId
+      }
+    })
 
     if (userSubscription && userSubscription.stripeCustomerId) {
       const stripeSession = await stripe.billingPortal.sessions.create({
         customer: userSubscription.stripeCustomerId,
         return_url: settingsUrl,
-      });
+      })
 
-      return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+      return new NextResponse(JSON.stringify({ url: stripeSession.url }))
     }
 
     const stripeSession = await stripe.checkout.sessions.create({
@@ -37,19 +36,20 @@ export async function GET() {
       cancel_url: settingsUrl,
       payment_method_types: ["card"],
       mode: "subscription",
+      billing_address_collection: "auto",
       customer_email: user.emailAddresses[0].emailAddress,
       line_items: [
         {
           price_data: {
-            currency: "gbp",
+            currency: "USD",
             product_data: {
-              name: "Cloud Genius Pro",
-              description: "Unlimited AI creation",
+              name: "Genius Pro",
+              description: "Unlimited AI Generations"
             },
             unit_amount: 2000,
             recurring: {
-              interval: "month",
-            },
+              interval: "month"
+            }
           },
           quantity: 1,
         },
@@ -57,10 +57,11 @@ export async function GET() {
       metadata: {
         userId,
       },
-    });
+    })
 
-    return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+    return new NextResponse(JSON.stringify({ url: stripeSession.url }))
   } catch (error) {
-    console.log("[STRIPE_USER] Error: ", error);
+    console.log("[STRIPE_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
   }
-}
+};
